3aea519ab1ba7c82b7364f8f467a8cb187881651
Henri Idrovo <henri.idrovo@gmail.com>
2015-07-22 23:47:18 -0500

    added comment

--- main.py
+++ main.py
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+#Congratulations! You built a webapp that responds to user input!
+#
+#
 import webapp2
 
 form = """
